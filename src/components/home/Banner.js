d3a186470a8b7c2a5c9425221ef1e7f207935a53
AhmedAshraf6 <ahmedp76b@gmail.com>
2023-03-31 00:02:52 +0200

    edit advertice

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -3,8 +3,9 @@ import Link from 'next/link';
 export default function Banner() {
   return (
     <section className=''>
-      <div className='bg-[url("/advertice.jpg")]  bg-cover bg-top bg-no-repeat scale-x-[-1]'>
-        <div className='container scale-x-[-1]'>
+      {/* bg-[url("/advertice.jpg")]  bg-cover bg-top bg-no-repeat scale-x-[-1] */}
+      <div className='bg-gray-200'>
+        <div className='container '>
           <h2 className='head'>بيع ديسمبير فعال!</h2>
           <h2 className='head text-primary'>بخصم كبير</h2>
           <div className='flex flex-col sm:flex-row gap-x-3 gap-y-2 flex-wrap items-start sm:items-center mb-2'>
